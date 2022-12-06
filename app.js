ae126d6a1ac611e9963a3ae492ff4066cb5cdb6d
Adnan Riaz <100839052+adnanRiyaz@users.noreply.github.com>
2022-12-06 10:28:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+
+// Step 1: Fill in these three consts by selecting the right nodes by their HTML ID's
+const names = document.getElementById("name"); // Fill this in 
+const age = document.getElementById("age"); // Fill this in.
+const dogYears = document.getElementById("dogyears"); // Fill this in. 
+
+// Step 2. Set the values.
+// Example: myNode.innerText = "something"
+// Set `name` and `age` in here
+names.innerText = "David";
+age.innerText = "29";
+
+
+
+
+function showDogYears(age) {
+    // Step 3. Cast `age` as a number
+    dogYears.innerText = parseInt(age * 7);
+    return age * 7
+}
+
+// Step 4. Set the parameter of showDogyears()
+dogYears.innerText = showDogYears(2)
+
+// Step 5. Select all the .fake-input classes and give them the class of .real-input. This may take a little "googling" to find the solution, but that's half of what web development is all about. 
+const fakeInputs = document.querySelectorAll(".fake-input")
+fakeInputs.forEach(node => {
+    node.classList.add("real-input")
+})
+
+
+
+
+
